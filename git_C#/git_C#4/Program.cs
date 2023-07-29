556a3eb494e40304285c88a45378af874732872f
arch-2-00 <arch-2-00@mail.ru>
2023-07-29 18:53:30 +0300

    задание8

---
+++ git_C#/git_C#4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count<num)
+{
+    int what = count;
+    if ((what % 2) == 0)
+{
+    Console.WriteLine(what);
+}
+count++;
+}
