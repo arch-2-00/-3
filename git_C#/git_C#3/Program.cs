bf6c73e45a6ed7e2048ea6e013834e3b8216245d
arch-2-00 <arch-2-00@mail.ru>
2023-07-29 18:02:26 +0300

    задача 6

---
+++ git_C#/git_C#3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num % 2) == 0)
+{
+    Console.WriteLine(num);
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine(num);
+    Console.WriteLine("нечетное");
+}
