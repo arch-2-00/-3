3c384ed57eafa5e5168cbe8ce4b5deac14f13a8b
arch-2-00 <arch-2-00@mail.ru>
2023-07-29 16:43:37 +0300

    исправленное

--- git_C#/Program.cs
+++ git_C#/Program.cs
@@ -4,9 +4,15 @@ Console.Write("Введите число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1 > num2)
 {
+    Console.Write("max= ");
     Console.WriteLine(num1);
+    Console.Write("min= ");
+    Console.Write(num2);
 }
 else
 {
+    Console.Write("max= ");
     Console.WriteLine(num2);
+    Console.Write("min= ");
+    Console.Write(num1);
 }
