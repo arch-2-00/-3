49f21a92fdc9ffe4f815150977faef83706982ae
arch-2-00 <arch-2-00@mail.ru>
2023-07-29 16:31:27 +0300

    первое домашнее задание

--- git_C#/Program.cs
+++ git_C#/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
