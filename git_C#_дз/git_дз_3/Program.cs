5a302747e7cac2e4c9a97e368f828e11cd560964
arch-2-00 <arch-2-00@mail.ru>
2023-08-07 21:14:01 +0300

    ДЗ_3 задача 23

---
+++ git_C#_дз/git_дз_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count<=num)
+{
+    int what = count;
+    if ((what % 1) == 0)
+{
+    Console.WriteLine(Math.Pow(what,3));
+}
+count++;
+}
