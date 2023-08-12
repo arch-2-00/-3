af3126069b90266d5d8f70c0e9e2d2d8add4793b
arch-2-00 <arch-2-00@mail.ru>
2023-08-12 20:38:46 +0300

    ДЗ_4 задача 27

---
+++ git_C#_дз/git_дз_4.1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num/100;
+int num2 = num/10;
+int num3= num2 %10;
+int num4= num %10;
+Console.WriteLine(num1+num3+num4);
