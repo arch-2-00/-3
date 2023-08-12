af0ead86797e910e9be359e05141e43ded4f6798
arch-2-00 <arch-2-00@mail.ru>
2023-08-12 16:59:18 +0300

    ДЗ_4 задача 25

---
+++ git_C#_дз/git_дз_4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Math.Pow(num1,num2));
+
