d1ed84189dac2e4a1a284e400cd749cc051f37dc
arch-2-00 <arch-2-00@mail.ru>
2023-08-13 18:10:07 +0300

    ДЗ_5 задача 34

---
+++ git_C#_дз/git_дз_5/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array= new int [6];
+
+int number=0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(100,999);
+    System.Console.Write(array[i]+" ");
+
+    if (array[i]%2)
+
+    {
+        number= number+1;
+    }
+}
+System.Console.Write(number)
