741b79e4451d70c0a40d9d780ee688f29ab2be8d
arch-2-00 <arch-2-00@mail.ru>
2023-08-13 12:23:15 +0300

    ДЗ_4 задача 29

---
+++ git_C#_дз/git_дз_4.2/Program.cs
@@ -0,0 +1,34 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+int []GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int [Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+{
+    array[i] = random.Next(minValue, maxValue + 1);
+}
+return array;
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.WriteLine("]");
+}
+
+int Length = Prompt("Длинна массива");
+int min = Prompt("Начальное значение, для диопазона случайного числа");
+int max = Prompt("Конечное значение, для диопазона случайного числа");
+int[] array = GenerateArray(Length, min, max);
+PrintArray(array);
+
