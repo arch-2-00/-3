b0d837dd878439f68f6af03bc1e1b39763b70f93
arch-2-00 <arch-2-00@mail.ru>
2023-08-13 13:14:53 +0300

    Урок 6

---
+++ git_C#/git_C#5/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] array= new int [12];
+
+int sumNegNumber=0;
+int sumPosNumber=0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]= new Random().Next(-9,10);
+    System.Console.Write(array[i]+" ");
+
+    if (array[i]>0)
+    {
+        sumPosNumber=sumPosNumber+array[i];
+    }
+    else
+    {
+        sumNegNumber=sumNegNumber+array[i];
+    }
+}
+System.Console.WriteLine($"Сумма положительных = {sumPosNumber}, отрицательных = {sumNegNumber}");
